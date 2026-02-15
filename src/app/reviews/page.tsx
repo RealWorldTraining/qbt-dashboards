15666d55f62b433770d8ba60047c1b1e9b9d6a3c
Ruvaldt <ruvaldt@gmail.com>
2026-02-14 21:25:58 -0600

    Reduce spacing in reviews list - compress top/bottom padding and item gaps

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -393,9 +393,9 @@ export default function ReviewsPage() {
           {/* Right Side - Reviews (70% width, scrollable) */}
           <div className="w-[70%] flex flex-col">
             <Card className="flex-1 flex flex-col">
-              <CardContent className="pt-6 flex-1 flex flex-col">
+              <CardContent className="pt-2 flex-1 flex flex-col">
                 <div 
-                  className="space-y-4 overflow-y-auto pr-2 flex-1"
+                  className="space-y-2 overflow-y-auto pr-2 flex-1"
                   style={{ maxHeight: 'calc(100vh - 300px)' }}
                 >
                   {filteredReviews.length === 0 ? (
@@ -406,7 +406,7 @@ export default function ReviewsPage() {
                     filteredReviews.map((review, idx) => (
                       <div
                         key={idx}
-                        className="border-b pb-4 last:border-b-0"
+                        className="border-b pb-2 last:border-b-0"
                       >
                         <div className="flex items-center justify-between mb-2">
                           <div className="flex-1">
